5626be7549c2619b89e086f9978cfcbbf7640b59
Vladimir Fibe <motiw@icloud.com>
2023-09-10 14:32:01 +0600

    Initial Commit

---
+++ FifteenPuzzle/FifteenPuzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FifteenPuzzleApp.swift
+//  FifteenPuzzle
+//
+//  Created by Vladimir Fibe on 10.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct FifteenPuzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
